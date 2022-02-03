09ffb5dc4277c601de40bb1057fce2b7e1444b73
Victor Lobato <victorsilva123190@gmail.com>
2022-02-03 14:39:20 -0300

    Adicionando UserService

---
+++ spring-security-jwt/src/main/java/com/victorlobato/springsecurityjwt/service/UserService.java
@@ -0,0 +1,21 @@
+package com.victorlobato.springsecurityjwt.service;
+
+import com.victorlobato.springsecurityjwt.model.User;
+import com.victorlobato.springsecurityjwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private PasswordEncoder encoder;
+    public void createUser(User user){
+        String pass = user.getPassword();
+        //criptografando antes de salvar no banco
+        user.setPassword(encoder.encode(pass));
+        repository.save(user);
+    }
+}
